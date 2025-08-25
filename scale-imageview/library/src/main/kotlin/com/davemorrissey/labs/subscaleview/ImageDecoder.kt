bc79be99799ac05ff8d5e10c70d1f0d8744a7685
mayurparmar23 <104304335+mayurparmar23@users.noreply.github.com>
2025-08-25 16:45:50 +0530

    Delete scale-imageview directory

--- scale-imageview/library/src/main/kotlin/com/davemorrissey/labs/subscaleview/ImageDecoder.kt
+++
@@ -1,9 +0,0 @@
-package com.davemorrissey.labs.subscaleview
-
-import android.content.Context
-import android.graphics.Bitmap
-import android.net.Uri
-
-interface ImageDecoder {
-    fun decode(context: Context, uri: Uri): Bitmap
-}
